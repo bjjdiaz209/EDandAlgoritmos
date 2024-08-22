d1d413ee613d8c8c7c3880d5d92227ad5f121a48
Juan209 <quintanajuancito34@gmail.com>
2024-08-21 22:55:52 -0400

    quicksort, merge sort

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,25 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array; // Caso base: listas de 0 o 1 elemento ya están ordenadas
+}
 
+let pivote = array[array.length - 1]; // Seleccionamos el último elemento como pivote
+let izquierda = []; // Sublista de elementos menores que el pivote
+let derecha = []; // Sublista de elementos mayores que el pivote
+
+for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < pivote) {
+        izquierda.push(array[i]); // Elementos menores que el pivote
+    } else {
+        derecha.push(array[i]); // Elementos mayores que el pivote
+    }
+}
+
+// Llamada recursiva a quickSort para las sublistas y combinación con el pivote
+return [...quickSort(izquierda), pivote, ...quickSort(derecha)];
+//return array;
 }
 
 function mergeSort(array) {
@@ -14,6 +32,41 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+}
+
+// Dividimos el array en dos mitades
+const mitad = Math.floor(array.length / 2);
+const izquierda = array.slice(0, mitad);
+const derecha = array.slice(mitad);
+
+// Ordenamos recursivamente cada mitad
+const sortedIzquierda = mergeSort(izquierda);
+const sortedDerecha = mergeSort(derecha);
+
+// Fusionamos las mitades ordenadas
+return merge(sortedIzquierda, sortedDerecha);
+}
+
+function merge(izquierda, derecha) {
+let resultado = [];
+let i = 0;
+let j = 0;
+
+// Comparamos los elementos de las dos sublistas y los fusionamos en orden
+while (i < izquierda.length && j < derecha.length) {
+    if (izquierda[i] < derecha[j]) {
+        resultado.push(izquierda[i]);
+        i++;
+    } else {
+        resultado.push(derecha[j]);
+        j++;
+    }
+}
+
+// Agregamos los elementos restantes, si los hay
+return resultado.concat(izquierda.slice(i)).concat(derecha.slice(j));
 
 }
 
