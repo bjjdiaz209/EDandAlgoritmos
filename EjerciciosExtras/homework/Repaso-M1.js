6c4f4476aa82697bf98c32f11a20bce9b84b43d8
Juan209 <quintanajuancito34@gmail.com>
2024-08-27 17:33:45 -0400

    se implemento las funcines del repaso

--- EjerciciosExtras/homework/Repaso-M1.js
+++ EjerciciosExtras/homework/Repaso-M1.js
@@ -16,8 +16,18 @@ const {
 
 var countArray = function(array) {
     // Tu código aca:
-    
-}
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+       if (Array.isArray(array[i])) {
+        sum+=countArray(array[i]);//sum = sum +respuesta de counArray(array[i])
+       } else {
+        sum+=array[i];
+       }
+        
+    }
+  return sum;  
+};
 
 
 // Implementar la función countProps: a partir de un objeto en el cual cada propiedad puede contener
@@ -39,7 +49,18 @@ var countArray = function(array) {
 
 var countProps = function(obj) {
     // Tu código aca:
-
+    let count = 0;
+    //for in -> "recorrer objetos"
+    for (const prop  in obj) {
+        count++;
+        if (typeof obj[prop] === 'object') {
+            //los arrays devuelven true
+            if (!Array.isArray(obj[prop])) {
+                count+=countProps(obj[prop]);
+            }            
+        }
+    }
+   return count;
 }
 
 
@@ -50,11 +71,26 @@ var countProps = function(obj) {
 //    Suponiendo que la lista actual es: Head --> [1] --> ['2'] --> [false] --> ['Franco']
 //    lista.changeNotNumbers();
 //    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
+// modifico la lista que tengo
+//devuelvo la cantidad de cambios ya hechos ![integer//numero]
 
 LinkedList.prototype.changeNotNumbers = function(){
     // Tu código aca:
+    //Number[('hola')] === NaN -> ! isNaN(value) => true // false
+    //Number('hola') ->> NaN
+    //Number('1')->> 1
+    let count = 0;
+    let current = this.head;
+    while (current) {
+        if (isNaN(Number(current.value))) {
+            count++;
+            current.value = 'Kiricocho';
+        }
+        current = current.next;
+    }
+    return count;
 
-}
+};
 
 
 // Implementar la función mergeQueues que a partir de dos queues recibidas por parametro
@@ -67,8 +103,20 @@ LinkedList.prototype.changeNotNumbers = function(){
 
 var mergeQueues = function(queueOne, queueTwo) {
     // Tu código aca:
-
-}
+    let queue = new Queue();//{array: []}
+    //array1 = []
+    //array2 = []
+    //firstElement = 5
+    //secondElement = undefined
+    // queue = [1,6,2,7,3,8,4,9,5]
+    while (queueOne.size() || queueTwo.size()) {
+        let firstElement = queueOne.dequeue();//me devuelve el primer elemnto de array 1
+        let secondElement = queueTwo.dequeue();//me devuelve el primer elemento del array 2
+        if(firstElement)queue.enqueue(firstElement);//firstElement && queue.
+        if(secondElement)queue.enqueue(secondElement);
+    }
+    return queue;
+};
 
 
 // Implementar la funcion closureMult que permita generar nuevas funciones que representen
@@ -82,19 +130,30 @@ var mergeQueues = function(queueOne, queueTwo) {
 
 var closureMult = function(multiplier) {
     // Tu código aca:
+    return function(num){
+        return num*multiplier;
+    }
 
-}
+};
 
 // Implementar el método sum dentro del prototype de BinarySearchTree
 // que debe retornar la suma total de los valores dentro de cada nodo del arbol
 BinarySearchTree.prototype.sum = function() {
     // Tu código aca:
+    if(!this.right && !this.left) return this.value;
+    if(!this.right && this.left) return this.value + this.left.sum();
+    if(!this.left && this.right) return this.value + this.right.sum();
+    if(this.left && this.right) return this.value + this.left.sum() + this.right.sum();
 
-}
+
+
+
+
+};
 
 module.exports = {
     countArray,
     countProps,
     mergeQueues,
     closureMult
-}
+};
